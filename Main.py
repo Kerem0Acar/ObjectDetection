95807c0bd3c68e4459851e41d92d35cbe7375ad1
mortvivant <softwaremin.aydin@gmail.com>
2025-03-24 15:48:30 +0300

    commit 1

--- Main.py
+++ Main.py
@@ -1,11 +1,2 @@
-import yeni
+print("HELLO WORLD")
 
-print("Bacinia")
-print("Ananı arıyorum")
-print("amıınnaakkeee")
-
-kerem1 = yeni.Kerem("güzel","yaralı")
-kerem2 = yeni.Kerem("devasa","vurgun")
-
-print(kerem1.knee)
-print(kerem2.butt)
--- yeni.py
+++
@@ -1,5 +0,0 @@
-class Kerem:
-
-    def __init__(self,butt,knee):
-        self.butt = butt
-        self.knee = knee
