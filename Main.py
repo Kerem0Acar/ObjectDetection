401dd928a561e2196e63a55449ab4147fe45b371
Mehmet Emin AYDIN <softwaremin.aydin@gmail.com>
2025-03-26 09:28:30 +0300

    commit 1

--- Main.py
+++ Main.py
@@ -1,2 +1,20 @@
-print("HELLO WORLD")
+import cv2
 
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Kamera açılamadı!")
+    exit()
+
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+
+    cv2.imshow("Kamera", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
