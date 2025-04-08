d49004813842ca42658bd9a7bb2844b6ac1d2b0b
mortvivant <softwaremin.aydin@gmail.com>
2025-04-08 22:25:14 +0300

    commit opencv

--- Main.py
+++ Main.py
@@ -1,2 +1,20 @@
-print("HELLO WORLD")
+import cv2
 
+cam = cv2.VideoCapture(0)
+
+if not cam.isOpened():
+    print("Kamera açılamadı!")
+    exit()
+
+while True:
+    success, frame = cam.read()
+    if not success:
+        break
+
+    cv2.imshow("Kamera", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
