6608681f23877768b36505b1a67da98aed93fa34
Kerem Acar <kemracr@gmail.com>
2025-04-25 17:19:05 +0300

    Adding to table in csv format

---
+++ AddNodepad.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import os
+from datetime import datetime
+
+notepad = pd.DataFrame(columns=['Date', 'Note'])
+file_path = "notepad.csv"
+
+def createCSV():
+    if checkNotepad():
+        print("Creating new CSV format")
+    else:
+        notepad.to_csv(file_path, index= False)
+
+def checkNotepad():
+    if os.path.exists(file_path):
+        print(f"{file_path} is already exist" )
+
+
+
+def addNode(noteText):
+    date_str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    new_note = {'Date': date_str, 'Note': noteText}
+    global notepad
+    notepad = notepad._append(new_note,ignore_index=True)
+    notepad.to_csv("notepad.csv",index=False)
--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
-import cv2
+import  cv2
+import AddNodepad
 
 cap = cv2.VideoCapture(0)
 
@@ -7,6 +8,8 @@ if not cap.isOpened():
     exit()
 cv2.namedWindow("Kamera",cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("Kamera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+AddNodepad.createCSV()
+AddNodepad.addNode("Phone")
 
 while True:
     success, frame = cap.read()
