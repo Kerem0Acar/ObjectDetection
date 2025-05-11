83275961baf682700e2ee21fc065bf0bb67dd862
Mehmet <softwaremin.aydin@gmail.com>
2025-05-11 13:43:08 +0300

    getting value from model is done

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import  cv2
 import AddNotepad
 from ultralytics import YOLO
+import time
 
 cap = cv2.VideoCapture(0)
 
@@ -11,7 +12,6 @@ model3 = r"v3-yolo11n-200-epoches/best.pt"
 
 model = YOLO("yolo11n.pt")
 
-
 if not cap.isOpened():
     print("Camera didn't open")
     exit()
@@ -20,8 +20,6 @@ cv2.namedWindow("Kamera",cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("Kamera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 AddNotepad.createCSV()
-AddNotepad.addNote("Phone", 0.84)
-
 
 while True:
     success, frame = cap.read()
@@ -30,6 +28,12 @@ while True:
 
     results = model.predict(frame, conf=0.5,verbose=False)
     annotated_frame = results[0].plot()
+
+    for box in results[0].boxes:
+        class_id = int(box.cls[0])
+        class_name = model.names[class_id]
+        confidence = float(box.conf[0])
+        AddNotepad.addNote(class_name,confidence)
 
     cv2.imshow("Kamera",annotated_frame)
 
