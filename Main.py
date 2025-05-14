c93a365c3b29360cfadc5bb059fc2cfd75f859ca
Mehmet <softwaremin.aydin@gmail.com>
2025-05-14 08:18:46 +0300

    yolov12 added to master and updating directoru names

--- Main.py
+++ Main.py
@@ -5,10 +5,10 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-model1 = r"v1-yolo8n-100-epoches/best.pt"
-model2 = r"v2-yolo11n-150-epoches/best.pt"
-model3 = r"v3-yolo11n-200-epoches/best.pt"
-model4 = r"v4-yolo11n-150-epoches/best.pt"
+model1 = r"v1-yolo8n-100-epoches(Phone)/best.pt"
+model2 = r"v2-yolo11n-150-epoches(Phone)/best.pt"
+model3 = r"v3-yolo11n-200-epoches(Phone and Glasses)/best.pt"
+model4 = r"v4-yolo11n-150-epoches(ToothBrush Sneakers Phone)/best.pt"
 
 
 model = YOLO(model4)
