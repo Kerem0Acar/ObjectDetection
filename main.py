76b922010f3dece917b6239054d00b465492461c
Mehmet <@mortvivant>
2025-05-17 16:05:23 +0300

    update main

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import uıPart
 
-model1 = r"v1-yolo8n-100-epoches(Phone)/best.pt"
-model2 = r"v2-yolo11n-150-epoches(Phone)/best.pt"
-model3 = r"v3-yolo11n-200-epoches(Phone and Glasses)/best.pt"
-model4 = r"v4-yolo11n-150-epoches(ToothBrush Sneakers Phone)/best.pt"
-
 if __name__ == "__main__":
     uıPart.start()
